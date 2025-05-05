6b856375df0bcbadb9d2676da09f341622fae83e
Demon-ut <anthony.goasdoue@ynov.com>
2025-05-05 12:22:46 +0200

    Ajout des fichiers principaux du gitignore

---
+++ serveur/serveur.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/html/homepage.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
