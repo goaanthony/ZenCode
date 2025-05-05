dfbcc3fb208ecf87ec899746270382464020b963
Demon-ut <anthony.goasdoue@ynov.com>
2025-05-05 16:23:40 +0200

    fix filesend to render

--- backend/route/edit.js
+++ backend/route/edit.js
@@ -3,8 +3,7 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../frontend/html/edit.html'));
+    res.render(path.join(__dirname, '../../frontend/html/edit.html'));
 });
 
 module.exports = router;
-//res.render
