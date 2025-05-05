608ce5ce07348c6b4a08ebaa5f7fa0e59ae2e680
gtimothe <timothe.guinard@ynov.com>
2025-05-05 16:13:52 +0200

    Fix pls :3

--- backend/route/home.js
+++ backend/route/home.js
@@ -3,7 +3,7 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../frontend/html/homepage.html'));
+    res.sendFile(path.join(__dirname, '../../frontend/html/homepage.ejs'));
 });
 
 module.exports = router;
